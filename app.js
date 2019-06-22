0c585fac36f68c06e6db914a58c5ee042ab771fe
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-06-22 19:23:27 +0300

    2

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ app.get(`/greeting`, (req, res) => {
   res.send(`Hey 'dere world!`);
 });
 // Starting our server on port 4000
-let port = process.env.port || 4000;
+let port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on ${port}`));
