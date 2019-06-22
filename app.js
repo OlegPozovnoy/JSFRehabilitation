157c5c9d3d27742f9627470da7b93b9fda1d5932
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-06-23 00:22:06 +0300

    3

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const express = require("express");
 // Assigning Express to an app constant
 const app = express();
 // Creating our first route which is looking for requests
-app.get(`/`, (req, res) => {
-  // res is the response object
-  // Our response
-  res.send(`Home`);
-});
-// Creating our first route which is looking for requests
-app.get(`/greeting`, (req, res) => {
-  // Our response
-  res.send(`Hey 'dere world!`);
-});
+
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+const routes = require("routes");
+app.use("/", routes);
+
 // Starting our server on port 4000
 let port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on ${port}`));
