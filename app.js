48a6d10334f07eff86217d3813f8e038763ad8da
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-06-22 18:43:38 +0300

    2

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// Our imported libraries
+const express = require("express");
+// Assigning Express to an app constant
+const app = express();
+// Creating our first route which is looking for requests
+app.get(`/`, (req, res) => {
+  // res is the response object
+  // Our response
+  res.send(`Home`);
+});
+// Creating our first route which is looking for requests
+app.get(`/greeting`, (req, res) => {
+  // Our response
+  res.send(`Hey 'dere world!`);
+});
+// Starting our server on port 4000
+let port = process.env.port ? process.env.port : 4000;
+app.listen(port, () => console.log("Listening on 4000"));
